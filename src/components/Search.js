e872b351ceb3e2741036453e80b1d281ba7b67d3
Angeline-Sharon <jnangelinesharon@gamil.com>
2024-01-31 19:16:22 +0530

    Created successfully

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+//Search Component
+function Search()
+{
+  return(
+      <div className="searchbar">
+        <input type="text" placeholder="Search"></input>
+      </div>
+
+  )
+}
+
+export default Search
